deef527bba2d0a1af394a5d3b2445d5d24e47715
Shreyash019 <shreyashgaur1@gmail.com>
2026-03-12 00:58:53 +0530

    chore: add comments to clarify exports in index.ts

--- packages/postgres-connector-ts/src/index.ts
+++ packages/postgres-connector-ts/src/index.ts
@@ -1,3 +1,6 @@
+// Postgres Connector (Direct)
 export { PostgresConnector } from "./PostgresConnector";
+// Postgres Prisma Connector
 export { createPrismaUrl as PrismaUrl } from "./PrismaConnector";
+// Postgres TypeOrm Connector
 export { createTypeOrmConfig } from "./TypeOrmConnector";
